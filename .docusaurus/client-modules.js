54984dc00eb78fcb0db7f4bca2746df119ea1543
Atanda1 <atandadave@gmail.com>
2025-03-06 18:59:27 +0100

    fix: remove docusaurus folder

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/david/Documents/projects/paycrest-docs/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/david/Documents/projects/paycrest-docs/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/david/Documents/projects/paycrest-docs/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/david/Documents/projects/paycrest-docs/docs/src/css/custom.css"),
-];
